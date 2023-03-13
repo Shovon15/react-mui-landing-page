ae71475c99f72f4da8852f8f18b6c972773952c9
Shovon15 <shovonrnbd@gmail.com>
2023-03-14 01:59:47 +0600

    .

---
+++ src/Pages/Contact/TypescriptFile.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const TypescriptFile = () => {
+    return (
+        <Box>
+            <Typography>Typescript</Typography>
+        </Box>
+    );
+};
+
+export default TypescriptFile;
---
+++ src/Pages/Contact/file.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const File = () => {
+    return (
+        <Box>
+            <Typography variant="h2">file</Typography>
+        </Box>
+    );
+};
+
+export default File;
